06a0ee2a15dd295a5df388a28db8a703e9521951
arfanan <siti.marfuah@dac-solution.com>
2016-02-19 02:38:44 -0800

    commit show list Tasks

--- SPReportGenerator/SPReportGenerator/Default.aspx.cs
+++ SPReportGenerator/SPReportGenerator/Default.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Client;
 
 namespace SPReportGenerator
 {
@@ -11,7 +14,12 @@ namespace SPReportGenerator
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Username : mca.developer@eceos.com
+            //Password : Dafo4806
+            using (var site = new SPSite("https://eceos2.sharepoint.com/sites/mca-dev"))
+            {
+                MessageBox.Show(site.Url);
+            }
         }
     }
 }
