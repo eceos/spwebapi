a90bfbcebe7e8a2bfa0e69d417eb7bf6dc53fff5
Rahadian D Dewandono <gusdewa@gmail.com>
2016-02-20 07:10:05 +0700

    Use SharePoint Client

--- SPReportGenerator/SPReportGenerator/Default.aspx.cs
+++ SPReportGenerator/SPReportGenerator/Default.aspx.cs
@@ -5,21 +5,82 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Microsoft.SharePoint;
 using Microsoft.SharePoint.Client;
+using System.Security;
 
 namespace SPReportGenerator
 {
+
     public partial class _Default : Page
     {
+        string CurUrl = "";
+        int width = 0;
+        int height = 0;
+
+        private string userName = "sp.services@eceos.com";
+        private string password = "Raja0432";
+
+        private string checkNull(object item)
+        {
+            try
+            {
+                return item.ToString();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private DateTime? checkNullDateTime(object item)
+        {
+            try
+            {
+                return new DateTime?(Convert.ToDateTime(item.ToString()));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        Microsoft.SharePoint.Client.ListItemCollection getList(string listName)
+        {
+            CurUrl = "https://eceos2.sharepoint.com/sites/mca-dev";
+            using (ClientContext context = new ClientContext(CurUrl))
+            {
+                SecureString secureString = new SecureString();
+                password.ToList().ForEach(secureString.AppendChar);
+                context.Credentials = new SharePointOnlineCredentials(userName, secureString);
+                Site site = context.Site;
+                context.Load(site);
+                context.ExecuteQuery();
+
+                List byTitle = context.Web.Lists.GetByTitle(listName);
+                CamlQuery query = CamlQuery.CreateAllItemsQuery();
+                Microsoft.SharePoint.Client.ListItemCollection clientObject = byTitle.GetItems(query);
+                context.Load<Microsoft.SharePoint.Client.ListItemCollection>(clientObject);
+                context.ExecuteQuery();
+                return clientObject;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Username : mca.developer@eceos.com
-            //Password : Dafo4806
-            using (var site = new SPSite("https://eceos2.sharepoint.com/sites/mca-dev"))
+            var list = getList("Tasks");
+            string toDisplay = string.Empty;
+
+            foreach (var item in list)
             {
-                MessageBox.Show(site.Url);
+                string boldStart = item["StartDate"].Equals(item["DueDate"]) ? "<strong>" : string.Empty;
+                string boldEnd = item["StartDate"].Equals(item["DueDate"]) ? "<strong>" : string.Empty;
+
+                toDisplay += string.Format("Task Name {0}{1}{2}, Start Date {3}, End Date {4}\n", boldStart, 
+                    item["Title"], boldEnd, item["StartDate"], item["DueDate"]);
             }
+
+            Label1.Text = toDisplay;
         }
     }
 }
