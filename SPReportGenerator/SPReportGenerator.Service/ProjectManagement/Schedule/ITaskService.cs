0822b2fffbcb33b8b9bd4b725ff0c712e4a29214
Rahadian D Dewandono <gusdewa@gmail.com>
2016-02-20 19:44:30 +0700

    Put example

---
+++ SPReportGenerator/SPReportGenerator.Service/ProjectManagement/Schedule/ITaskService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SPReportGenerator.Model.ProjectManagement.Schedule;
+
+namespace SPReportGenerator.Service.ProjectManagement.Schedule
+{
+    public interface ITaskService
+    {
+        bool CreateTask(Task task);
+
+        IEnumerable<Task> GetAllTask();
+
+        IEnumerable<Task> GetAllTaskNotCompleted();
+
+
+    }
+}
